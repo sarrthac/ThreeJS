aa72a4b26b4afe9173b7a85e91c08ba3e6752f15
Sarthak Zalte <sarthakzalte@gmail.com>
2023-06-18 11:38:08 +0530

    inital commit

---
+++ hellothreejs/main.js
@@ -0,0 +1,51 @@
+import * as THREE from "three";
+import "./style.css"
+//Scene
+const scene = new THREE.Scene();
+
+//Create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+  height: window.innerHeight,
+  width: window.innerWidth
+}
+
+
+//Lights
+const light = new THREE.PointLight(0xffffff,1,100)
+light.position.set(0,10,10)
+scene.add(light)
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width/sizes.height,
+  0.1,
+  100
+  );
+camera.position.z = 20;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width,sizes.height)
+renderer.render(scene,camera)
+
+
+
+//Resize
+window.addEventListener("resize", () => {
+
+  //Updates the sizes everytime the window is resized
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+})
+
